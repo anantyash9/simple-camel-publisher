0917a000fee7caf8249e5d88e0179fa086eb9d6b
Anant Yash Pande <anantyash9@gmail.com>
2021-03-23 15:52:25 +0530

    multi topic publishing

--- src/main/java/avs/manager/demo/GeneratorBean.java
+++ src/main/java/avs/manager/demo/GeneratorBean.java
@@ -2,11 +2,15 @@ package avs.manager.demo;
 
 import java.util.Random;
 
+import javax.annotation.PostConstruct;
+
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 
 @Component("GeneratorBean")
+
 public class GeneratorBean {
 
     private int counter;
@@ -18,20 +22,64 @@ public class GeneratorBean {
     @Value("${ava}")
     private String ava;
     
-    private String[] messages= {ava,mas,ava};
+    @Value("${carrier1}")
+    private String carrier1;
+    @Value("${carrier2}")
+    private String carrier2;
+    @Value("${carrier3}")
+    private String carrier3;
+    @Value("${carrier4}")
+    private String carrier4;
+    @Value("${carrier5}")
+    private String carrier5;
+    @Value("${carrier6}")
+    private String carrier6;
+    @Value("${carrier7}")
+    private String carrier7;
+    @Value("${carrier8}")
+    private String carrier8;
+    @Value("${carrier9}")
+    private String carrier9;
+    @Value("${carrier10}")
+    private String carrier10;
+    
 
-    public String getRandomMessage(String body) {
-//    	System.out.println(counter);
-//        return "Flight "+String.valueOf(++counter);
-    	String[] messages= {ava,mas,ava};
-    	Random random = new Random();
-    	int index = random.nextInt(messages.length);
-    	return  messages[index];
+    
+    private String[] messages;
+    private String[] carriers;
+    private String[] randomMessages=new String[1000];;
+    
+    @PostConstruct  
+    public void postConstruct()
+    {	
+    	 messages= new String[] {avs,mas,ava};
+    	 carriers = new String[] {carrier1,carrier2,carrier3,carrier4,carrier5,carrier6,carrier7,carrier8,carrier9,carrier10};
+    	 Random random = new Random();
+    	 for (int j=0;j<1000;j++) {
+    	    	int index = random.nextInt(messages.length);
+    	    	String message = messages[index];
+    	    	index = random.nextInt(carriers.length);
+    	    	String carrier = carriers[index];
+    	    	int lineCounts=random.nextInt(5);
+    	    	for(int i = 0 ; i<=lineCounts ; i++) {
+    	    		message+=carrier+RandomStringUtils.random(17, true, true)+"\n";
+    	    	}
+    	    	randomMessages[j]=message;
+    		 
+    		 
+    	 }
+    	 System.out.println(randomMessages[333]);
     }
+    public String getRandomMessage() {
+    	Random random = new Random();
+    	int index = random.nextInt(randomMessages.length);
+    	return randomMessages[index] ;
+    }
+    
 
-    public String hashIt(String body) {
-//    	System.out.println(body.hashCode()%3);
-        return String.valueOf(Math.abs(body.hashCode()%3));
+    public String getCarrier(String body) {
+    	String lines[] = body.split("\\r?\\n");
+        return lines[3].substring(0,2);
     }
 
 }
--- src/main/java/avs/manager/demo/MessagePublisher.java
+++ src/main/java/avs/manager/demo/MessagePublisher.java
@@ -16,8 +16,10 @@
  */
 package avs.manager.demo;
 
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 
@@ -32,15 +34,52 @@ public class MessagePublisher extends RouteBuilder {
     // we can use spring dependency injection
     @Autowired
     GeneratorBean generatorBean;
+    @Autowired
+    private Environment env;
 
     @Override
     public void configure() throws Exception {
         // start from a timer
         from("timer:hello?delay=-1").routeId("publisher")
-                // and call the bean
+                // generate a random message body
                 .bean(generatorBean, "getRandomMessage")
-                .setHeader("GooglePubsubConstants.ORDERING_KEY",constant("{{carrier}}"))
-        .to("{{pub-sub-topic}}");
+                // get carrier id and set ordering key to carrier id
+                .setHeader("GooglePubsubConstants.ORDERING_KEY",method(generatorBean,"getCarrier"))
+                //route the message to pub sub topic based on carrier id
+                .choice()
+                .when(header("GooglePubsubConstants.ORDERING_KEY").isEqualTo(env.getProperty("carrier1")))
+                .toD("{{pub-sub-prefix}}{{pub-sub-carrier1}}{{pub-sub-parameters}}")
+                
+                .when(header("GooglePubsubConstants.ORDERING_KEY").isEqualTo(env.getProperty("carrier2")))
+                .toD("{{pub-sub-prefix}}{{pub-sub-carrier2}}{{pub-sub-parameters}}")
+                
+                .when(header("GooglePubsubConstants.ORDERING_KEY").isEqualTo(env.getProperty("carrier3")))
+                .toD("{{pub-sub-prefix}}{{pub-sub-carrier3}}{{pub-sub-parameters}}")
+                
+                .when(header("GooglePubsubConstants.ORDERING_KEY").isEqualTo(env.getProperty("carrier4")))
+                .toD("{{pub-sub-prefix}}{{pub-sub-carrier4}}{{pub-sub-parameters}}")
+                
+                .when(header("GooglePubsubConstants.ORDERING_KEY").isEqualTo(env.getProperty("carrier5")))
+                .toD("{{pub-sub-prefix}}{{pub-sub-carrier5}}{{pub-sub-parameters}}")
+                
+                .when(header("GooglePubsubConstants.ORDERING_KEY").isEqualTo(env.getProperty("carrier6")))
+                .toD("{{pub-sub-prefix}}{{pub-sub-carrier6}}{{pub-sub-parameters}}")
+                
+                .when(header("GooglePubsubConstants.ORDERING_KEY").isEqualTo(env.getProperty("carrier7")))
+                .toD("{{pub-sub-prefix}}{{pub-sub-carrier7}}{{pub-sub-parameters}}")
+                
+                .when(header("GooglePubsubConstants.ORDERING_KEY").isEqualTo(env.getProperty("carrier8")))
+                .toD("{{pub-sub-prefix}}{{pub-sub-carrier8}}{{pub-sub-parameters}}")
+                
+                .when(header("GooglePubsubConstants.ORDERING_KEY").isEqualTo(env.getProperty("carrier9")))
+                .toD("{{pub-sub-prefix}}{{pub-sub-carrier9}}{{pub-sub-parameters}}")
+                
+                .when(header("GooglePubsubConstants.ORDERING_KEY").isEqualTo(env.getProperty("carrier10")))
+                .toD("{{pub-sub-prefix}}{{pub-sub-carrier10}}{{pub-sub-parameters}}")
+//        .to("{{pub-sub-topic}}");
+//        .to("stream:out");
+                .end()
+                .to("log:Throughput Logger?level=INFO&groupInterval=10000&groupDelay=60000&groupActiveOnly=false");
 
     }
 
